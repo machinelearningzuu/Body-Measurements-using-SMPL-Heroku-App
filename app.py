a1f1843ec787d6b0a30fb94b7cc4975c23857000
1zuu <isurualagiyawanna9717@gmail.com>
2021-05-01 00:20:37 +0530

    change the host

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def predict():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=False, use_reloader=False)
+    app.run(debug=True, host=heroku_url, port=5000, threaded=False, use_reloader=False)
